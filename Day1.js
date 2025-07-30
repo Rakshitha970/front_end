1d9815696b6bf5afd375b94cf1dd34b8506ee010
unknown <rakshithapulicharla483@gmail.com>
2025-07-30 18:55:11 +0530

    Day2

--- Day1.js
+++ Day1.js
@@ -1,1 +1,37 @@
-console.log("jg7yhjhuyh");
+let a="abc";
+let k=12;
+let k1=14;
+console.log(k);
+
+//arrar 
+let b=[12,13,23,89 ,"hello"];
+console.log(b[1]);
+console.log(b[21]);// its prints undefined
+console.log(b[2]);//Read
+b[3]=34;//update
+b[16]=45;//create 
+console.log(b);
+console.log(b.length);
+
+//shallow copy only for non primitive datatype
+let r=b;
+r[1]=21;
+console.log(r[1]);
+console.log(b[1]);//shallow copy
+
+//object key, value pair
+let obj={name:"Rakshii" ,class:"frontend" ,marks:[34,56,78]};
+//Object.seal(obj);
+//Object.freeze(obj);
+obj["name"]="krishna";
+obj["name"]="Deepa";//updated
+obj["name1"]="rakshii";//adding new key
+console.log(obj);
+console.log(obj["name1"]);
+console.log(obj["marks"]);
+
+//shallow copy
+let obj1=obj;
+obj1[1]="Dilliraj";
+console.log(obj1[1]);
+console.log(obj[1]);
---
+++ Day2.js
@@ -0,0 +1,54 @@
+let a=50;
+let b=a++ + a++ + ++a + a + ++a;
+console.log(a);
+console.log(b);
+
+
+
+let c=24;
+let d=50;
+let e=c+d;
+console.log(e);
+
+
+console.log( "4" * 8);
+console.log("7a" *2);// its prints NAN
+console.log("true" *7);//its prints NAN
+
+console.log(a * null);//its prints 0
+console.log(null * null);//its prints 0
+console.log(null * undefined);//its prints NAN
+console.log(4 * null);//its prints 0
+console.log(4 +undefined);//its prints NAN
+
+console.log(2/0);
+console.log(0.9+0.1);
+console.log(2.1+ 4.2);
+console.log(2.1 + 2.2);
+console.log(4 -undefined);
+
+
+
+console.log(2.1 - 4.2);
+console.log(2.1 - 2.2);
+
+
+console.log(2.1 * 2.2);
+
+console.log([1,2,3] *[2,3,4]);
+console.log([2,1] + [2,2]);
+
+console.log({ name: "rakshii"} * {name1:"Deepa" });
+
+
+console.log("2" > "12");
+console.log("undefined" <"null");//its prints false
+console.log("undefined" >"undefined");//its prints false
+
+console.log("undefined" >"null");//its prints true
+console.log("undefined" <"null");//its prints false
+console.log("null" >"null");//its prints false
+
+console.log("a" <"c");//its prints false
+
+
